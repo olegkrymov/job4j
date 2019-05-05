922d876609ca50025d0c4c7a656024cbd5252023
Oleg Krymov <krymov.o@gmail.com>
2019-05-05 13:38:29 +0200

    1.3. JavaDoc. Code convention[#137835]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,17 @@
 package ru.job4j.calculate;
 
+/**
+* Class Calculate
+* @author Oleg Krymov
+* @since 05.05.2019
+* version 1
+*/
 public class Calculate {
-	
+
+/**
+* Main.
+* @param args - args.
+*/	
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,9 @@
+/**
+* Package for calculate task.
+*
+* @author Oleg Krymov (krymov.o@gmail.com)
+* @version $Id$
+* @since 0.1
+*/
+
+package ru.job4j.calculate;
