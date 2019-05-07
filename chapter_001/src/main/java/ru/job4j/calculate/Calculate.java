ae553c50da2575e0b9a8bee264d467b993400068
Oleg Krymov <krymov.o@gmail.com>
2019-05-07 21:28:20 +0200

    2.5. Инспектор кода IDEA.[#137786]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,27 +1,27 @@
 package ru.job4j.calculate;
 
 /**
-* Class Calculate
-* @author Oleg Krymov
-* @since 05.05.2019
-* version 1
-*/
+ * Class Calculate
+ * @author Oleg Krymov
+ * @since 05.05.2019
+ * version 1
+ */
 public class Calculate {
 
     /**
      * Main.
      * @param args - args.
-     */	
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-	}
-	
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
     /**
      * Method echo.
      * @param name Your name.
      * @return Echo plus your name.
      */
-     public String echo(String name) {
-         return "Echo, echo, echo : " + name;
- }	
+    public String echo(String name) {
+        return "Echo, echo, echo : " + name;
+    }
 }
