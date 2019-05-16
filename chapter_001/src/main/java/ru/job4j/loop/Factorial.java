d2b9c3c2a6487aa628fe050a9fcd60b0d597dfc6
Oleg Krymov <krymov.o@gmail.com>
2019-05-16 22:49:01 +0200

    5.2. Создать программу, вычисляющую факториал.[#137803]

--- chapter_001/src/main/java/ru/job4j/loop/Factorial.java
+++ chapter_001/src/main/java/ru/job4j/loop/Factorial.java
@@ -16,9 +16,6 @@ public class Factorial {
      */
     public int calc(int n) {
         int result = 1;
-        if (n == 0) {
-            return result;
-        }
         for (int i = 1; i <= n; i++) {
             result *= i;
         }
